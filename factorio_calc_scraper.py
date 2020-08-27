fe33162c7df0a9b310a1f9d2766008de43dabf7f
Marshall Ferguson <moferg01@louisville.edu>
2020-08-27 11:22:15 -0400

    added TODOs

---
+++ factorio_calc_scraper.py
@@ -0,0 +1,11 @@
+# Factorio Calc Scraper - Marshall Ferguson - 8/2020
+
+# TODO: Figure out template for url to get different results from calc
+
+# TODO: Request web page
+
+# TODO: Parse through HTML to get link to .csv
+
+# TODO: Pull data from .csv
+
+# TODO: Manipulate data from .csv to show total number of assemblers needed
